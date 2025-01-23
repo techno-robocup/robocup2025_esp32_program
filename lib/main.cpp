32208d3c1597aa844d85db4026fe72c5a6a0d528
K10-K10 <keito.tottori@gmai.com>
2025-01-23 08:36:35 +0000

    Make LEDPRogram

---
+++ lib/main.cpp
@@ -0,0 +1,33 @@
+#include <Wire.h>
+#include <iostream>
+#define I2C_ADDRESS 0x04
+#define LED_PIN 2
+
+void setup()
+{
+    Serial.begin(115200);
+    Wire.begin(0x08);
+    priMode(LED_PIN, OUTPUT);
+    Wire.onReceive(receiveEvent);
+    Wire.onRequest(requestEvent);
+}
+
+void loop()
+{
+    wire.requestFrom(I2C_ADDRESS, 32);
+
+    int bytecnt = 0;
+    while (Wire.available())
+    {
+        byte data = Wire.read();
+        bytecnt++;
+    }
+
+    for (int i = 0; i < bytecnt; i++)
+    {
+        digitalWrite(LED_PIN, HIGH);
+        delay(500);
+        digitalWrite(LED_PIN, LOW);
+        delay(500);
+    }
+}
