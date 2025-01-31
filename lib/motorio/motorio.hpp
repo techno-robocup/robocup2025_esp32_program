ee30602a9b2f32a0b5dfed0e52d659afbcf94595
rotarymars <s.goto2050@gmail.com>
2025-01-31 11:44:38 +0900

    ci

---
+++ lib/motorio/motorio.hpp
@@ -0,0 +1,19 @@
+#ifndef __ROBOT__MOTORIO__HPP
+#define __ROBOT__MOTORIO__HPP 1
+#include <cstdlib>
+#include <Arduino.h>
+namespace robot {
+  class motorio {
+    public:
+    motorio(const std::int8_t&,const std::int8_t&,int*);
+    void motor_brake();
+    void set_speed(int);
+    void run_speed();
+    private:
+    std::int8_t IN1_PIN, IN2_PIN;
+    int speed;
+    int *speed_array;
+    std::uint8_t speed_variations = 10;
+  };
+}
+#endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
 #include <Wire.h>
+#include <motorio.hpp>
 void receiveEvent(int);
 void requestEvent();
 template <typename... Args>
@@ -9,40 +10,51 @@ inline void debugprintln(Args... args);
 
 #define _DEBUG 1
 
-constexpr std::int8_t IN1 = 32;
-constexpr std::int8_t IN2 = 33;
-constexpr std::int8_t TL = 4;
-constexpr std::int8_t TR = 2;
+constexpr std::int8_t LIN1 = 32;
+constexpr std::int8_t LIN2 = 33;
+constexpr std::int8_t RIN1 = 26;
+constexpr std::int8_t RIN2 = 27;
 int motor_speed[2] = {-1, -1};
 bool recieved = false;
 int cmd = 0x10;
-
+int left_speed_array[41] = {
+    200, 190, 180, 170, 160, 150, 140, 130, 120, 110, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10,
+    0,
+    10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200
+  },
+  right_speed_array[41] = {
+    200, 190, 180, 170, 160, 150, 140, 130, 120, 110, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10,
+    0,
+    10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200
+  };
+robot::motorio left_motor(LIN1, LIN2, left_speed_array);
+robot::motorio right_motor(RIN1, RIN2, right_speed_array);
 void setup()
 {
   Serial.begin(115200);
   Wire.begin(0x08);
   Wire.onReceive(receiveEvent);
   Wire.onRequest(requestEvent);
-  pinMode(IN1, OUTPUT);
-  pinMode(IN2, OUTPUT);
+  // pinMode(IN1, OUTPUT);
+  // pinMode(IN2, OUTPUT);
 }
 
 void loop()
 {
-  if (motor_speed[0] == -1 || motor_speed[1] == -1)
-  {
-    return;
+  if (recieved) {
+    debugprintln("RECIEVED!",motor_speed[0], motor_speed[1]);
+    recieved = false;
+    left_motor.set_speed(motor_speed[0]);
+    right_motor.set_speed(motor_speed[1]);
   }
-  analogWrite(IN1, motor_speed[0]);
-  analogWrite(IN2, motor_speed[1]);
-  debugprintln("RECIEVED!", motor_speed[0], " ", motor_speed[1]);
-  recieved = false;
+  left_motor.run_speed();
+  right_motor.run_speed();
 }
 
 void receiveEvent(int byteNum)
 {
   debugprintln(byteNum);
-  if (byteNum < 3)
+  if (byteNum < 5)
   {
     while (Wire.available())
     {
@@ -55,8 +67,18 @@ void receiveEvent(int byteNum)
     cmd = Wire.read();
     if (cmd == 0x00)
     {
-      motor_speed[0] = Wire.read();
-      motor_speed[1] = Wire.read();
+      int posneg = Wire.read();
+      int recievednum = Wire.read();
+      if (posneg == 0x01)
+        motor_speed[0] = -recievednum;
+      else
+        motor_speed[0] = recievednum;
+      posneg = Wire.read();
+      recievednum = Wire.read();
+      if (posneg == 0x01)
+        motor_speed[1] = -recievednum;
+      else
+        motor_speed[1] = recievednum;
     }
     else
     {
@@ -78,8 +100,6 @@ void requestEvent()
     Wire.write(0x00); // request raspberrypi to stop sending
   if (!recieved)
     Wire.write(0x01); // Allow raspberrypi to send
-  Wire.write(touchRead(TL));
-  Wire.write(touchRead(TR));
   return;
 }
 
