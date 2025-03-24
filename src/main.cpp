74d5b944094aa7a719f9707114f001fb9a188687
rotarymars <s.goto2050@gmail.com>
2025-03-24 16:27:19 +0900

    removed unnecessary variables

--- src/main.cpp
+++ src/main.cpp
@@ -17,16 +17,6 @@ constexpr std::int8_t RIN2 = 27;
 int motor_speed[2] = {-1, -1};
 bool recieved = false;
 int cmd = 0x10;
-int left_speed_array[41] = {
-    200, 190, 180, 170, 160, 150, 140, 130, 120, 110, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10,
-    0,
-    10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200
-  },
-  right_speed_array[41] = {
-    200, 190, 180, 170, 160, 150, 140, 130, 120, 110, 100, 90, 80, 70, 60, 50, 40, 30, 20, 10,
-    0,
-    10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150, 160, 170, 180, 190, 200
-  };
 robot::motorio left_motor(LIN1, LIN2);
 robot::motorio right_motor(RIN1, RIN2);
 void setup()
@@ -35,8 +25,6 @@ void setup()
   Wire.begin(0x08);
   Wire.onReceive(receiveEvent);
   Wire.onRequest(requestEvent);
-  // pinMode(IN1, OUTPUT);
-  // pinMode(IN2, OUTPUT);
 }
 
 void loop()
