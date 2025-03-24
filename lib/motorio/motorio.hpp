5cfd324e2b5bd6d96e2094e4c4580186be8b87b4
rotarymars <s.goto2050@gmail.com>
2025-03-24 16:43:21 +0900

    removed unnecessary functions and variables

--- lib/motorio/motorio.hpp
+++ lib/motorio/motorio.hpp
@@ -6,13 +6,11 @@ namespace robot {
   class motorio {
     public:
     motorio(const std::int8_t&,const std::int8_t&);
-    void motor_brake();
     void set_speed(int);
     void run_speed();
     private:
     std::int8_t IN1_PIN, IN2_PIN;
     int speed;
-    const std::uint8_t speed_variations = 20;
   };
 }
 #endif
