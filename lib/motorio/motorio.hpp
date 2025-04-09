9e665c6d0b0de79b7eb52ac0872b5849e9d4cc97
rotarymars <s.goto2050@gmail.com>
2025-04-09 23:13:52 +0900

    lib/motorio/motorio.hpp: Formatted file

--- lib/motorio/motorio.hpp
+++ lib/motorio/motorio.hpp
@@ -1,13 +1,14 @@
 #ifndef __ROBOT__MOTORIO__HPP
 #define __ROBOT__MOTORIO__HPP 1
 #include <Arduino.h>
-class MOTORIO{
-  public:
+class MOTORIO {
+public:
   MOTORIO();
-  MOTORIO(const std::int8_t&);
-  MOTORIO& operator=(const MOTORIO&)=default;
-  void run_msec(const int&);
-  private:
+  MOTORIO(const std::int8_t &);
+  MOTORIO &operator=(const MOTORIO &) = default;
+  void run_msec(const int &);
+
+private:
   std::int8_t PIN;
 };
 #endif
