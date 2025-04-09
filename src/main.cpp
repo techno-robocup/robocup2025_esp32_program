9ae6dd47c1c2b901a0c5de451d8915a7cac685e4
k10-k10 <keito.tottori@gmail.com>
2025-04-09 18:36:17 +0900

    Fix main.cpp.
    add thread.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <Arduino.h>
 #include <Wire.h>
 #include <motorio.hpp>
+
 void receiveEvent(int);
 void requestEvent();
 template <typename... Args>
@@ -9,53 +10,74 @@ template <typename... Args>
 inline void debugprintln(Args... args);
 
 #define _DEBUG 1
+
 int motor_speed[2] = {0, 0};
+int motor_speed2[2] = {0, 0};
 
 int motor_pin = 32;
 int fb_pin = 33;
+int motor2_pin = 26;
+int fb2_pin = 27;
 
 bool recieved = false;
 int cmd = 0x10;
+
 MOTORIO motor;
-void setup(){
+MOTORIO motor2;
+
+void MotorControlTask(void *pvParameters)
+{
+  while (true)
+  {
+    motor.run_msec(motor_speed[0]);
+    motor2.run_msec(motor_speed[1]);
+    vTaskDelay(20 / portTICK_PERIOD_MS);
+  }
+}
+
+void SerialDebugTask(void *pvParameters)
+{
+  while (true)
+  {
+    debugprint("motor1: ", motor_speed[0], " / ");
+    debugprintln("motor2: ", motor_speed[1]);
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+  }
+}
+
+void setup()
+{
   Serial.begin(115200);
   Wire.begin(0x08);
   Wire.onReceive(receiveEvent);
   Wire.onRequest(requestEvent);
+
   motor = MOTORIO(motor_pin);
+  motor2 = MOTORIO(motor2_pin);
   pinMode(fb_pin, INPUT);
+
+  xTaskCreatePinnedToCore(
+      MotorControlTask,
+      "MotorTask",
+      2048,
+      NULL,
+      1,
+      NULL,
+      0);
+
+  xTaskCreatePinnedToCore(
+      SerialDebugTask,
+      "DebugTask",
+      2048,
+      NULL,
+      1,
+      NULL,
+      1);
 }
-
-
-void loop()
-{
-  long lastmil=millis();
-  while(millis()-lastmil<2000){
-    motor.run_msec(1000);
-    delay(20);
-    Serial.println("1000");
-    Serial.println(analogRead(fb_pin));
-  }
-  lastmil=millis();
-  while(millis()-lastmil<2000){
-    motor.run_msec(1500);
-    delay(20);
-    Serial.println("1500");
-    Serial.println(analogRead(fb_pin));
-  }
-  lastmil=millis();
-  while(millis()-lastmil<3000){
-    motor.run_msec(2000);
-    delay(20);
-    Serial.println("2000");
-    Serial.println(analogRead(fb_pin));
-  }
-}
-
 
 void receiveEvent(int byteNum)
 {
-  std::int8_t a,b;
+  std::int8_t a, b;
   debugprintln(byteNum);
   if (byteNum < 5)
   {
@@ -71,15 +93,21 @@ void receiveEvent(int byteNum)
     if (cmd == 0x00)
     {
       a = Wire.read();
-      if(a == 0){
+      if (a == 0)
+      {
         motor_speed[0] = Wire.read();
-      }if(a == 1){
+      }
+      if (a == 1)
+      {
         motor_speed[0] = -1 * Wire.read();
       }
       b = Wire.read();
-      if(b == 0){
+      if (b == 0)
+      {
         motor_speed[1] = Wire.read();
-      }if(b == 1){
+      }
+      if (b == 1)
+      {
         motor_speed[1] = -1 * Wire.read();
       }
     }
@@ -100,10 +128,14 @@ void receiveEvent(int byteNum)
 void requestEvent()
 {
   if (recieved)
-    Wire.write(0x00); // request raspberrypi to stop sending
+    Wire.write(0x00);
   if (!recieved)
-    Wire.write(0x01); // Allow raspberrypi to send
+    Wire.write(0x01);
   return;
+}
+void loop()
+{
+  // Not used.
 }
 
 #ifdef _DEBUG
