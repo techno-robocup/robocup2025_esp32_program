5394ef01562d5d32cd949e2a4335e3076c5165ec
rotarymars <s.goto2050@gmail.com>
2025-04-10 17:40:40 +0900

    lib/my_mutex: Removed my_mutex

--- lib/my_mutex/my_mutex.cpp
+++
@@ -1,6 +0,0 @@
-#include <my_mutex.hpp>
-Mymutex::Mymutex(portMUX_TYPE *mux) {
-  _mutex = mux;
-  portENTER_CRITICAL(_mutex);
-}
-Mymutex::~Mymutex() { portEXIT_CRITICAL(_mutex); }
--- lib/my_mutex/my_mutex.hpp
+++
@@ -1,8 +0,0 @@
-#include <Arduino.h>
-class Mymutex {
-  public:
-  Mymutex(portMUX_TYPE* mux);
-  ~Mymutex();
-  private:
-  portMUX_TYPE *_mutex;
-};
