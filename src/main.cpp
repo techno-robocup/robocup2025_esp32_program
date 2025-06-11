902b098306c7801c2aebebec039023cbc92eb169
rotarymars <s.goto2050@gmail.com>
2025-06-11 16:18:56 +0900

    added debug print message

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,12 @@
 
 void setup() {
   Serial.begin(9600);
+  long time = millis();
   while (true) {
+    if (millis() - time > 1000) {
+      Serial.println("Test message");
+      time = millis();
+    }
     while (!Serial.available());
     String str = Serial.readStringUntil('\n');
     if (str == "[RASPI] READY?") {
