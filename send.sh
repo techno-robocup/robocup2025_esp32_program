c85b921ed2d915cb5b9c9c53c7a380d072219018
rotarymars <s.goto2050@gmail.com>
2025-06-11 16:26:52 +0900

    corrected program

--- send.sh
+++ send.sh
@@ -1,4 +1,4 @@
 find . -name "*.cpp" -o -name "*.h" -o -name "*.c" -o -name "*.hpp" | while read file; do
-    rsync -av --delete --exclude .direnv/ --exclude .git/ "$file" robo@roboberry.local:robocup2025_esp32_program
+    rsync -av --delete --exclude .direnv/ --exclude .git/ "$file" robo@roboberry.local:robocup2025_esp32_program/$file
 done
 
