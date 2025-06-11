fd321a28d9e60b65c61effed58482e0c0cdd5a90
rotarymars <s.goto2050@gmail.com>
2025-06-11 15:50:04 +0900

    initialized project

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,20 @@
 #include <Arduino.h>
 
 void setup() {
-
+  Serial.begin(9600);
+  while(true){
+    while(!Serial.available());
+    String str = Serial.readStringUntil('\n');
+    if (str == "[RASPI] READY?"){
+      Serial.println("[ESP32] READY");
+    }
+    str = Serial.readStringUntil('\n');
+    if (str == "[RASPI] READY CONFIRMED"){
+      break;
+    }
+  }
 }
 
 void loop() {
-  
+
 }
