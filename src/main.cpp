14b046a8c2fad1a50ada7d7402616c0605be6efa
rotarymars <s.goto2050@gmail.com>
2025-06-12 16:35:38 +0900

    decided to create it as a lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,9 @@
 #include <Arduino.h>
 
-bool isReady = false;
-
 void setup() {
   Serial.begin(9600);
-  while (true) {
-    if (Serial.available()) {
-      String str = Serial.readStringUntil('\n');
-      if (str == "[RASPI] READY?") {
-        Serial.println("[ESP32] READY");
-      } else if (str == "[RASPI] READY CONFIRMED") {
-        isReady = true;
-      }
-    }
-  }
 }
 
-void loop() {}
+void loop() {
+
+}
