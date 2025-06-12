e29a3de28e6b19ee7ed88c9f452eb09cf0e17361
rotarymars <s.goto2050@gmail.com>
2025-06-12 16:14:58 +0900

    moved to setup() for serial initialization

--- src/main.cpp
+++ src/main.cpp
@@ -3,15 +3,20 @@
 bool isReady = false;
 unsigned long lastMessageTime = 0;
 
-void setup() { Serial.begin(9600); }
-
-void loop() {
-  if (Serial.available()) {
-    String str = Serial.readStringUntil('\n');
-    if (str == "[RASPI] READY?") {
-      Serial.println("[ESP32] READY");
-    } else if (str == "[RASPI] READY CONFIRMED") {
-      isReady = true;
+void setup() {
+  Serial.begin(9600);
+  while(true) {
+    if (Serial.available()) {
+      String str = Serial.readStringUntil('\n');
+      if (str == "[RASPI] READY?") {
+        Serial.println("[ESP32] READY");
+      } else if (str == "[RASPI] READY CONFIRMED") {
+        isReady = true;
+      }
     }
   }
+}
+
+void loop() {
+  
 }
