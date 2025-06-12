c39bf16a1f5f2415d7114e65a81637918230cb77
rotarymars <s.goto2050@gmail.com>
2025-06-12 16:12:09 +0900

    removed: Removed test message

--- src/main.cpp
+++ src/main.cpp
@@ -6,10 +6,6 @@ unsigned long lastMessageTime = 0;
 void setup() { Serial.begin(9600); }
 
 void loop() {
-  if (millis() - lastMessageTime > 1000) {
-    Serial.println("Test message");
-    lastMessageTime = millis();
-  }
   if (Serial.available()) {
     String str = Serial.readStringUntil('\n');
     if (str == "[RASPI] READY?") {
