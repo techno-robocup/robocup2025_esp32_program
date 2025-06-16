e7d071001628463b764960d28177d49877b83e75
K10-K10 <keito.tottori@gmail.com>
2025-06-16 17:36:08 +0900

    read Message

--- src/main.cpp
+++ src/main.cpp
@@ -7,5 +7,43 @@ SerialIO serial;
 void setup() { serial.init(); }
 
 void loop() {
-  if (!serial.isMessageAvailable()) continue;
+  if (!serial.isMessageAvailable()) return;
+
+  Message msg = serial.receiveMessage();
+  String message = msg.getMessage();
+
+  if (message.startsWith("Motor ")) {
+    message = message.substring(6);
+    if (message.length() == 19) {
+      int tyre_values[4];
+      int idx = 0;
+
+      while (message.length() > 0 && idx < 4) {
+        int spaceIndex = message.indexOf(' ');
+        if (spaceIndex == -1) {
+          tyre_values[idx++] = message.toInt();
+          break;
+        } else {
+          tyre_values[idx++] = message.substring(0, spaceIndex).toInt();
+          message = message.substring(spaceIndex + 1);
+        }
+      }
+
+      if (idx == 4) {
+        Serial.printf("motor values = %d %d %d %d\n", tyre_values[0], tyre_values[1],
+                      tyre_values[2], tyre_values[3]);  // TODO: clate motor fn
+      } else {
+        Serial.println("Error: less than 4 values");
+      }
+    } else {
+      Serial.println("Error: motor message must be 4-digit");
+    }
+  }
+  if (message.startsWith("Rescue ")) {
+    message = message.substring(7);
+    if (message.length() == 5) {  // NOTE: ID Rescue arm_angle wire(0,1)
+      int arm_values = message.substring(0, 4).toInt();
+      bool wire = message[5];
+    }
+  }
 }
