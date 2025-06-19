120b3feb3823a0afe785c09d4c11ec5af8ec95fb
rotarymars <s.goto2050@gmail.com>
2025-06-19 17:28:32 +0900

    locked resource for tyre_values

--- src/main.cpp
+++ src/main.cpp
@@ -50,13 +50,16 @@ void loop() {
       int idx = 0;
       // TODO: Optimize the coe by not copying the String
       while (message.length() > 0 && idx < 2) {
-        int spaceIndex = message.indexOf(' ');
-        if (spaceIndex == -1) {
-          tyre_values[idx++] = message.toInt();
-          break;
-        } else {
-          tyre_values[idx++] = message.substring(0, spaceIndex).toInt();
-          message = message.substring(spaceIndex + 1);
+        if (xSemaphoreTake(motor_sem, portMAX_DELAY) == pdTRUE) {
+          int spaceIndex = message.indexOf(' ');
+          if (spaceIndex == -1) {
+            tyre_values[idx++] = message.toInt();
+            xSemaphoreGive(motor_sem);
+            break;
+          } else {
+            tyre_values[idx++] = message.substring(0, spaceIndex).toInt();
+            message = message.substring(spaceIndex + 1);
+          }
         }
       }
     } else {
